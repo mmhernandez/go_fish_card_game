e038b43f8e6cf483a152b7f9b30c158244925d25
mmhernandez <mmdangelo11@gmail.com>
2023-04-19 20:57:03 -0700

    Build file structure

---
+++ flask_app/__init__.py
@@ -0,0 +1,4 @@
+from flask import Flask 
+
+app = Flask(__name__)
+app.secret_key = "nGoJCtYZgdgk7y1s5i61"
---
+++ flask_app/controllers/game_controller.py
@@ -0,0 +1,9 @@
+from flask_app import app
+
+@app.route("/")
+def game():
+    pass
+
+@app.route("/start_game")
+def start_game():
+    pass
