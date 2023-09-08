a6693dcadbfadbf2c24ea726ac38e01caad85474
Melissa Hernandez <mmdangelo11@gmail.com>
2023-09-08 14:48:50 -0700

    Update cancellation button styling

--- flask_app/static/js/script_modal.js
+++ flask_app/static/js/script_modal.js
@@ -1,6 +1,7 @@
 let btn = document.getElementById('cancel-button');
 let confirm = document.getElementById('confirm-cancel');
 let button = document.getElementById('confirm-button');
+let cancel = document.getElementById('cancel');
 
 
 btn.onclick = function() {
@@ -11,6 +12,10 @@ btn.onclick = function() {
 window.onclick = function (event) {
     if (event.target == button) {
         button.style.display = "none";
+    }
+    if (event.target == cancel) {
+        btn.style.display = 'block';
+        confirm.style.display = 'none';
     }
 }
 
